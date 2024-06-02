b1203eb2e0b6339e0fcb55c4cae9a88fed059645
Kooreis <kooreis.official@gmail.com>
2024-06-02 12:46:36 -0500

    Added a method 'next' to the MovingAverage class. This method takes a new value as input, checks if the queue is full, and if so, removes the oldest value from the queue and the sum. It then adds the new value to the queue and the sum, and returns the new average.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,8 @@
-class MovingAverage {
-    constructor(size) {
-        this.size = size;
-        this.queue = [];
-        this.sum = 0;
+next(val) {
+        if (this.queue.length === this.size) {
+            this.sum -= this.queue.shift();
+        }
+        this.queue.push(val);
+        this.sum += val;
+        return this.sum / this.queue.length;
     }
-}
